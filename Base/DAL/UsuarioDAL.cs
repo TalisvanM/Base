4bc248c982cfb621173beceb6a6fea38208176a8
TalisvanM <90804179+TalisvanM@users.noreply.github.com>
2022-05-10 21:54:24 -0300

    Finalizado o método Inserir no UsuarioDAL
    Amanhã criaremos o banco de dados e testaremos o insert na tabela usuário

---
+++ Base/DAL/UsuarioDAL.cs
@@ -0,0 +1,61 @@
+﻿using Model;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_InserirUsuario";
+
+                cmd.Parameters.Add (new SqlParameter("@Ativo", SqlDbType.Bit)
+                {
+                    Value = _usuario.Ativo
+                });
+
+                cmd.Parameters.Add (new SqlParameter("@NomrUsuario", SqlDbType.VarChar)
+                {
+                    Value = _usuario.NomeUsuario
+                });
+
+                cmd.Parameters.Add(new SqlParameter("@Senha", SqlDbType.VarChar)
+                {
+                    Value = _usuario.Senha
+                });
+
+                cmd.Parameters.Add(new SqlParameter("@ID", SqlDbType.Int)
+                {
+                    Value = _usuario.Id
+                });
+
+
+                cn.Open();
+                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return _usuario;
+            }
+            catch(SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
